cb754547f6f66c43560bbd064f581d43d29f780b
PistonYang <pistonyang@gmail.com>
2019-07-01 15:36:22 +0800

    Add a non-label dataset to load images without labels.

---
+++ torchtoolbox/data/datasets.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Author  : DevinYang(pistonyang@gmail.com)
+
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class NonLabelDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.transform = transform
+        self.items = os.listdir(root_dir)
+        self.items = [os.path.join(root_dir, f) for f in self.items]
+
+    def __len__(self):
+        return len(self.items)
+
+    def __getitem__(self, item):
+        img = Image.open(self.items[item]).convert('RGB')
+        if self.transform:
+            img = self.transform(img)
+        return img
