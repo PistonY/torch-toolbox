a7aec23af6f5dfe14e43549ddaf9725c36c862e8
DevinYang <pistonyang@gmail.com>
2020-01-08 11:05:01 +0800

    add utils.py to /data/__init__

--- torchtoolbox/data/__init__.py
+++ torchtoolbox/data/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # @Author  : DevinYang(pistonyang@gmail.com)
+from .utils import *
 from .lmdb_dataset import *
 from .datasets import *
 from .dataprefetcher import DataPreFetcher
--- torchtoolbox/data/utils.py
+++ torchtoolbox/data/utils.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__all__ = ['decode_img_from_buf', 'cv_loader']
+__all__ = ['decode_img_from_buf', 'pil_loader', 'cv_loader']
 
 import cv2
 import six
