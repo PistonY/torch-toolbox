ef4ddc42383e47af547cdff23b8dbb4352dbcc8c
sailfish009 <sailfish009@gmail.com>
2020-04-04 11:23:54 +0900

    Update convert_lmdb.py

--- torchtoolbox/tools/convert_lmdb.py
+++ torchtoolbox/tools/convert_lmdb.py
@@ -33,10 +33,12 @@ def generate_lmdb_dataset(data_set: Dataset, save_dir: str, name: str,
                           num_workers=0, max_size_rate=1.0, write_frequency=5000):
     data_loader = DataLoader(data_set, num_workers=num_workers, collate_fn=lambda x: x)
     num_samples = len(data_set)
+    
+    if os.path.exists(save_dir) == False:
+      os.mkdir(save_dir)
 
     lmdb_path = os.path.join(save_dir, '{}.lmdb'.format(name))
-    isdir = os.path.isdir(lmdb_path)
-    db = lmdb.open(lmdb_path, subdir=isdir,
+    db = lmdb.open(lmdb_path, subdir=False,
                    map_size=int(1099511627776 * max_size_rate),
                    readonly=False, meminit=True, map_async=True)
     txn = db.begin(write=True)
