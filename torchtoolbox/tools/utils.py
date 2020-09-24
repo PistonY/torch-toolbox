badc1d2229bc1560ce1059385b5e97ef9d89af4f
x.yang <pistonyang@gmail.com>
2020-09-24 18:06:21 +0800

    Update make_divisible algorithm, now same with tf repo.

--- torchtoolbox/tools/utils.py
+++ torchtoolbox/tools/utils.py
@@ -22,5 +22,15 @@ def check_dir(*path):
             os.mkdir(p)
 
 
-def make_divisible(x, divisible_by=8):
-    return int(math.ceil(x * 1. / divisible_by) * divisible_by)
+def make_divisible(v, divisible_by, min_value=None):
+    """
+    This function is taken from the original tf repo.
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    """
+    if min_value is None:
+        min_value = divisible_by
+    new_v = max(min_value, int(v + divisible_by / 2) // divisible_by * divisible_by)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisible_by
+    return new_v
