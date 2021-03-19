5ec7f34f9f2dfced316740a593b34ca65ac7c7b7
x.yang <xuyang@deepmotion.ai>
2021-03-19 15:29:32 +0800

    update Registry.

--- torchtoolbox/tools/registry.py
+++ torchtoolbox/tools/registry.py
@@ -15,24 +15,26 @@ class Registry:
             f"An object named '{name}' was already registered in '{self.name}' registry!"
         self._obj_map[name] = obj
 
-    def register(self, obj: object = None) -> Optional[object]:
+    def register(self, obj: object = None, name: str = None) -> Optional[object]:
         """Register the give object.
 
         Args:
             obj (object, optional): obj to register. Defaults to None.
-
+            name (str, optional): specific name for this obj.
         Returns:
             Optional[object]: ori obj.
         """
         if obj is None:
+
             def deco(func_or_class: object) -> object:
-                name = func_or_class.__name__
-                self._do_register(name, func_or_class)
+                _name = func_or_class.__name__ if name is None else name
+                self._do_register(_name, func_or_class)
                 return func_or_class
+
             return deco
 
-        name = obj.__name__
-        self._do_register(name, obj)
+        _name = obj.__name__ if name is None else name
+        self._do_register(_name, obj)
 
     def get(self, name: str) -> object:
         ret = self._obj_map.get(name)
