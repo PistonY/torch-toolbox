6bfdce8bbf3953ef34e1687abd727f192340e9f6
ghlai9665 <glai9665@gmail.com>
2021-10-30 11:16:12 -0500

    refactor CosineWarmupLr with creation of Scheduler class

--- torchtoolbox/optimizer/lr_scheduler.py
+++ torchtoolbox/optimizer/lr_scheduler.py
@@ -5,7 +5,31 @@ __all__ = ['CosineWarmupLr']
 from math import pi, cos
 from torch.optim.optimizer import Optimizer
 
-class CosineWarmupLr(object):
+class Scheduler(object):
+  def __init__(self):
+    raise NotImplementedError
+
+  def get_lr(self):
+    raise NotImplementedError
+
+  def state_dict(self):
+    """Returns the state of the scheduler as a :class:`dict`.
+
+    It contains an entry for every variable in self.__dict__ which
+    is not the optimizer.
+    """
+    return {key: value for key, value in self.__dict__.items() if key != 'optimizer'}
+
+  def load_state_dict(self, state_dict):
+    """Loads the schedulers state.
+
+    Arguments:
+        state_dict (dict): scheduler state. Should be an object returned
+            from a call to :meth:`state_dict`.
+    """
+    self.__dict__.update(state_dict)
+
+class CosineWarmupLr(Scheduler):
     """Cosine lr decay function with warmup.
 
     Lr warmup is proposed by `
@@ -62,22 +86,6 @@ class CosineWarmupLr(object):
         self.last_iter = last_iter
         self.step()
 
-    def state_dict(self):
-        """Returns the state of the scheduler as a :class:`dict`.
-
-        It contains an entry for every variable in self.__dict__ which
-        is not the optimizer.
-        """
-        return {key: value for key, value in self.__dict__.items() if key != 'optimizer'}
-
-    def load_state_dict(self, state_dict):
-        """Loads the schedulers state.
-
-        Arguments:
-            state_dict (dict): scheduler state. Should be an object returned
-                from a call to :meth:`state_dict`.
-        """
-        self.__dict__.update(state_dict)
 
     def get_lr(self):
         if self.last_iter < self.warmup_iters:
